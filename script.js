7d0ee795fb468559b294442a8991bb6a2a20d75d
ArchieAtOrigins <archie@originscreative.com>
2017-04-05 08:58:11 -0700

    Created listener on additembutton, created new li and attached input textContent and attached to ol

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-var addButton = document.getElementById("addButton");
+//* 
 
+let addButton = document.getElementById("addButton");
+let addItem = document.getElementById('addItem');
+let newItem = addItem.value;
 
-addButton.addEventListener('click', function() {
-
+//* Add listener to add button, create new item and add to list
+addButton.addEventListener('click',() => {
+	let tdList = document.getElementById('tdList');
+	let li = document.createElement('li');
+	li.textContent = addItem.value;
+	tdList.appendChild(li);
 });
+
