12fa293693deef37ff8f73c8812ecd5bc290d574
ArchieAtOrigins <archie@originscreative.com>
2017-04-06 09:06:05 -0700

    Created up, down, edit and remove button

--- script.js
+++ script.js
@@ -12,3 +12,40 @@ addButton.addEventListener('click',() => {
 	tdList.appendChild(li);
 });
 
+//* Create remove, up, down and edit buttons
+function createButtonSet(li) {
+	let upButton = document.createElement('button');
+	upButton.className = 'upBtn';
+	upButton.textContent = 'Up';
+	li.appendChild(upButton);
+
+	let downButton = document.createElement('button');
+	downButton.className = 'downBtn';
+	downButton.textContent = 'Down';
+	li.appendChild(downButton);
+
+	let editButton = document.createElement('button');
+	editButton.className = 'editBtn';
+	editButton.textContent = 'Edit';
+	li.appendChild(editButton);
+
+	let removeButton = document.createElement('button');
+	removeButton.className = 'removeBtn';
+	removeButton.textContent = 'Remove';
+	li.appendChild(removeButton);
+}
+
+//* Append to new items
+for(let i=0; i<tdList.length; i++) {
+	createButtonSet('');
+}
+
+//* Move items to completed list
+//* Move items up or down
+//* Edit items
+//* 
+
+//* Hide or Show lists
+
+
+
